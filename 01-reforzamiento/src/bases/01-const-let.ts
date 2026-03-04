4f9ac32c08f0a589c25029b0abbd32af880f6ce4
Piojoso <work.leonel.bongiovanni@outlook.com.ar>
2026-03-03 23:27:11 -0300

    01 - const and let

---
+++ 01-reforzamiento/src/bases/01-const-let.ts
@@ -0,0 +1,15 @@
+
+
+const firstName = "Leonel"
+let lastName = "Bongiovanni"
+
+console.log({ firstName, lastName })
+
+let containsCharL = firstName.includes('l')
+
+console.log({ containsCharL })
+
+const diceNumber = 5
+
+console.log({ diceNumber })
+
