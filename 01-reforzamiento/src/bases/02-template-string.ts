318c8b8cf661a7df0990f59596f457bf2123efa5
Piojoso <work.leonel.bongiovanni@outlook.com.ar>
2026-03-03 23:34:37 -0300

    01 - template strings

---
+++ 01-reforzamiento/src/bases/02-template-string.ts
@@ -0,0 +1,16 @@
+
+
+const firstName = 'Leonel'
+const lastName = 'Bongiovanni'
+
+const fakeLastName = "O'Neal"
+
+const fakeFullName = `${firstName} ${fakeLastName}`
+
+console.log(fakeFullName);
+
+console.log(firstName, lastName)
+
+
+
+
