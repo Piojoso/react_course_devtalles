0ad0264e4bf6a490fc923332fc06b7a643f37988
Piojoso <work.leonel.bongiovanni@outlook.com.ar>
2026-03-06 12:19:44 -0300

    01 - giphy response interface

--- 01-reforzamiento/src/bases/10-fetch-api.ts
+++ 01-reforzamiento/src/bases/10-fetch-api.ts
@@ -1,15 +1,22 @@
+import type { GiphyRandomResponse } from "../data/giphy.response"
+
 const API_KEY = import.meta.env.VITE_GIPHY_API_KEY
 
 const getGiphyData = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=&rating=g`)
 
+const createImageElement = (imageSrc: string) => {
+    const imgElement = document.createElement('img')
+    imgElement.src = imageSrc
+
+    document.body.append(imgElement)
+}
+
 getGiphyData
     .then((response) => response.json())
-    .then((data) => {
-        const imageUrl = data.data.images.original.url
+    .then(({ data }: GiphyRandomResponse) => {
+        // const imageUrl = data.data.images.original.url
+        const imageUrl = data.images.original.url
 
-        const imgElement = document.createElement('img')
-        imgElement.src = imageUrl
-
-        document.body.append(imgElement)
+        createImageElement(imageUrl)
     })
     .catch((error) => console.error(error))
