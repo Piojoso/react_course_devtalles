a42bee1f5f98556752d4b4e3a3142176f72cdbf1
Piojoso <work.leonel.bongiovanni@outlook.com.ar>
2026-03-07 18:50:29 -0300

    02 - showing list of elements

--- 02-first-steps/src/FirstStepsApp.tsx
+++ 02-first-steps/src/FirstStepsApp.tsx
@@ -1,14 +1,25 @@
 import { ItemCounter } from './shopping-cart/ItemCounter';
 
+interface ItemInCart {
+  productName: string;
+  quantity: number;
+}
+
+/* Please Gave!!, Allow us to purchase them here in LATAM */
+const itemsInCart: ItemInCart[] = [
+  { productName: 'Steam Deck', quantity: 1 },
+  { productName: 'Steam Machine', quantity: 1 },
+  { productName: 'Steam Controller', quantity: 2 },
+];
+
 export function FirstStepsApp() {
   return (
     <>
       <h1>Shopping Cart</h1>
 
-      {/* Please Gave!!, Allow us to purchase them here in LATAM */}
-      <ItemCounter name="Steam Deck" />
-      <ItemCounter name="Steam Machine" />
-      <ItemCounter name="Steam Controller" quantity={2} />
+      {itemsInCart.map(({ productName, quantity }) => (
+        <ItemCounter key={productName} name={productName} quantity={quantity} />
+      ))}
     </>
   );
 }
