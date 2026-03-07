91918e3371e73ccc3e33b609e5d81f4f7e949f75
Piojoso <work.leonel.bongiovanni@outlook.com.ar>
2026-03-07 17:19:36 -0300

    02 - printing different variables types on tsx

--- 02-first-steps/src/MyAwesomeApp.tsx
+++ 02-first-steps/src/MyAwesomeApp.tsx
@@ -1,8 +1,30 @@
+const firstName = 'Leonel';
+const lastName = 'Bongiovanni';
+
+const favoriteGames = [
+  'Portal 2',
+  'Hollow Knight',
+  'Need for speed Underground 2',
+];
+
+const isActive = false;
+
+const address = {
+  zipCode: 'abc123',
+  country: 'Argentina',
+};
+
 export function MyAwesomeApp() {
   return (
     <>
-      <h1>Leonel</h1>
-      <h3>Bongiovanni</h3>
+      <h1>{firstName}</h1>
+      <h3>{lastName}</h3>
+
+      <p>{favoriteGames.join(', ')}</p>
+
+      <h1>{isActive ? 'Activo' : 'Desactivado'}</h1>
+
+      <p>{JSON.stringify(address)}</p>
     </>
   );
 }
