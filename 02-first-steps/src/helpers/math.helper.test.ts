4adf5f4b4399ab6b290a364b6d1d97930fcee3b9
Piojoso <work.leonel.bongiovanni@outlook.com.ar>
2026-03-08 19:05:00 -0300

    03 - testing ui and coverage

--- 02-first-steps/src/helpers/math.helper.test.ts
+++ 02-first-steps/src/helpers/math.helper.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from 'vitest';
-import { add, multiply, subtract } from './math.helper';
+import { add, divide, multiply, subtract } from './math.helper';
 
 describe('add', () => {
   test('should add two positive numbers', () => {
@@ -85,5 +85,16 @@ describe('multiply', () => {
     const result = multiply(a, b);
 
     expect(result).toBe(a * b);
+  });
+
+  describe('divide', () => {
+    test('should divide two positive numbers', () => {
+      const a = 4;
+      const b = 2;
+
+      const result = divide(a, b);
+
+      expect(result).toBe(a / b);
+    });
   });
 });
