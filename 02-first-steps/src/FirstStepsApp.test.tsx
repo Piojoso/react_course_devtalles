e01770a9e698b9d428e13bd6415df64c6dd912a6
Piojoso <work.leonel.bongiovanni@outlook.com.ar>
2026-03-08 18:37:48 -0300

    03 - mocking components

--- 02-first-steps/src/FirstStepsApp.test.tsx
+++ 02-first-steps/src/FirstStepsApp.test.tsx
@@ -1,11 +1,23 @@
-import { render } from '@testing-library/react';
-import { describe, expect, test } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { describe, expect, test, vi } from 'vitest';
 import { FirstStepsApp } from './FirstStepsApp';
+
+vi.mock('./shopping-cart/ItemCounter.tsx', () => ({
+  ItemCounter: () => <div data-testid="ItemCounter" />,
+}));
 
 describe('FirstStepsApp.tsx', () => {
   test('should match snapshot', () => {
     const { container } = render(<FirstStepsApp />);
 
     expect(container).toMatchSnapshot();
+  });
+
+  test('should render the correct number of ItemCounter components', () => {
+    render(<FirstStepsApp />);
+
+    const itemCounterElements = screen.getAllByTestId('ItemCounter');
+
+    expect(itemCounterElements.length).toBe(3);
   });
 });
