0392fcb5b17b777e7afa7b75a166dfdf3efd73d8
Piojoso <work.leonel.bongiovanni@outlook.com.ar>
2026-03-10 09:36:59 -0300

    05 - cache managing manually

--- 03-gifs-app/src/gifs/hooks/useGifs.tsx
+++ 03-gifs-app/src/gifs/hooks/useGifs.tsx
@@ -2,12 +2,27 @@ import { useState } from "react";
 import type { Gif } from "../interfaces/gif.interface";
 import { getGifsByQuery } from "../actions/get-gifs-by-query";
 
+type CachedGif = Record<string, Gif[]>;
+
+const gifCache: CachedGif = {};
+
 export const useGifs = () => {
   const [gifs, setGifs] = useState<Gif[]>([]);
-
   const [previousSearches, setPreviousSearches] = useState<string[]>([]);
 
+  const loadFromCache = (term: string): boolean => {
+    if (gifCache[term]) {
+      setGifs(gifCache[term]);
+
+      return true;
+    }
+
+    return false;
+  };
+
   const handlePreviousSerchesClick = async (searchedTerm: string) => {
+    if (loadFromCache(searchedTerm)) return;
+
     const gifs = await getGifsByQuery(searchedTerm);
 
     setGifs(gifs);
@@ -18,12 +33,15 @@ export const useGifs = () => {
 
     if (searchedTerm === "") return;
     if (previousSearches.includes(searchedTerm)) return;
+    if (loadFromCache(searchedTerm)) return;
 
     setPreviousSearches([searchedTerm, ...previousSearches].splice(0, 5));
 
     const gifs = await getGifsByQuery(searchedTerm);
 
     setGifs(gifs);
+
+    gifCache[searchedTerm] = gifs;
   };
 
   const handleGetGifUrl = (url: string): string => {
