68e89d970ea7646f9070eeea3d07da11151ba6fa
Piojoso <work.leonel.bongiovanni@outlook.com.ar>
2026-03-11 17:36:42 -0300

    07 - recomendations with useEffect

--- 04-hooks-app/src/02 - useEffect/TrafficLightWithEffect.tsx
+++ 04-hooks-app/src/02 - useEffect/TrafficLightWithEffect.tsx
@@ -10,19 +10,30 @@ type Color = keyof typeof colorClasses;
 
 export const TrafficLightWithEffect = () => {
   const [light, setLight] = useState<Color>("red");
-  const [countDown, setCountDown] = useState(5);
+  const [countdown, setCountdown] = useState(5);
 
   useEffect(() => {
-    if (countDown === 0) return;
+    if (countdown === 0) return;
 
     const intervalId = setInterval(() => {
-      setCountDown((prev) => prev - 1);
+      setCountdown((prev) => prev - 1);
     }, 1000);
 
     return () => {
       clearInterval(intervalId);
     };
-  }, [countDown]);
+  }, [countdown]);
+
+  useEffect(() => {
+    if (countdown > 0) return;
+
+    setCountdown(5);
+
+    if (light === "red") setLight("green");
+    if (light === "green") setLight("yellow");
+    if (light === "yellow") setLight("red");
+    return;
+  }, [countdown, light]);
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
@@ -30,7 +41,13 @@ export const TrafficLightWithEffect = () => {
         <h1 className="text-white text-3xl font-thin">
           Semáforo con useEffect
         </h1>
-        <h2 className="text-white text-xl ">{countDown}</h2>
+        <h2 className="text-white text-xl ">{countdown}</h2>
+        <div className="w-64 bg-gray-700 rounded-full h-2">
+          <div
+            className="bg-blue-500 h-2 rounded-full transition-all duration-1000 ease-linear"
+            style={{ width: `${(countdown / 5) * 100}%` }}
+          ></div>
+        </div>
 
         <div
           className={`w-32 h-32 ${light === "red" ? colorClasses[light] : "bg-gray-500"} rounded-full`}
