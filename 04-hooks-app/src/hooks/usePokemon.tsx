8ff8d7900aa3effe6aaa05fea842b124e8a609a7
Piojoso <work.leonel.bongiovanni@outlook.com.ar>
2026-03-11 22:47:01 -0300

    07 - creating pokemon and counter hook.

---
+++ 04-hooks-app/src/hooks/usePokemon.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+interface Pokemon {
+  id: number;
+  name: string;
+  imageUrl: string;
+}
+
+interface Props {
+  id: number;
+}
+
+export const usePokemon = ({ id }: Props) => {
+  const [pokemon, setPokemon] = useState<Pokemon | null>(null);
+
+  const getPokemonById = async (id: number) => {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon/1");
+
+    const data = await response.json();
+
+    setPokemon({
+      id: id,
+      name: data.name,
+      imageUrl: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`,
+    });
+  };
+
+  return {
+    pokemon,
+  };
+};
