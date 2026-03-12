01328ecbd8d3bb5403c7689d21223dc5d159ab48
Piojoso <work.leonel.bongiovanni@outlook.com.ar>
2026-03-12 08:55:01 -0300

    07 - connecting custom hooks

--- 04-hooks-app/src/hooks/usePokemon.tsx
+++ 04-hooks-app/src/hooks/usePokemon.tsx
@@ -12,9 +12,12 @@ interface Props {
 
 export const usePokemon = ({ id }: Props) => {
   const [pokemon, setPokemon] = useState<Pokemon | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   const getPokemonById = async (id: number) => {
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon/1");
+    setIsLoading(true);
+
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
 
     const data = await response.json();
 
@@ -23,9 +26,18 @@ export const usePokemon = ({ id }: Props) => {
       name: data.name,
       imageUrl: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`,
     });
+
+    setIsLoading(false);
   };
+
+  useEffect(() => {
+    getPokemonById(id);
+  }, [id]);
 
   return {
     pokemon,
+    isLoading,
+
+    formatterId: id.toString().padStart(3, "0"),
   };
 };
