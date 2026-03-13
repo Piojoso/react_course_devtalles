f39775019baf7c30b8cab9e55770708864b23169
Piojoso <work.leonel.bongiovanni@outlook.com.ar>
2026-03-12 23:43:15 -0300

    08 - writting the reducer function

--- 04-hooks-app/src/05 - useReducer/reducer/tasks.reducer.ts
+++ 04-hooks-app/src/05 - useReducer/reducer/tasks.reducer.ts
@@ -20,5 +20,52 @@ export const taskReducer = (
   state: TaskState,
   action: TaskAction,
 ): TaskState => {
-  return state;
+  switch (action.type) {
+    case "ADD_TODO": {
+      const { todo_text } = action.payload;
+
+      const newTodo: Todo = {
+        id: Date.now(),
+        text: todo_text.trim(),
+        completed: false,
+      };
+
+      const newTodoList = [...state.todos, newTodo];
+
+      return {
+        ...state,
+        todos: newTodoList,
+        lenght: newTodoList.length,
+        pending: state.pending + 1,
+      };
+    }
+
+    case "TOGGLE_TODO": {
+      const { id } = action.payload;
+
+      const newTodoList = state.todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed };
+        }
+
+        return todo;
+      });
+
+      const completed = newTodoList.filter((t) => t.completed).length;
+      const pending = newTodoList.length - completed;
+
+      return { ...state, todos: newTodoList, completed, pending };
+    }
+
+    case "REMOVE_TODO": {
+      const { id } = action.payload;
+
+      const newTodoList = state.todos.filter((todo) => todo.id !== id);
+
+      return { ...state, todos: newTodoList, lenght: newTodoList.length };
+    }
+
+    default:
+      return state;
+  }
 };
