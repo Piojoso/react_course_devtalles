d87a93aa007f7e6b5591d4c21f2421b52d358e48
Piojoso <work.leonel.bongiovanni@outlook.com.ar>
2026-04-08 12:41:29 -0300

    09 - useCallback

--- 04-hooks-app/src/06 - memos/MemoHook.tsx
+++ 04-hooks-app/src/06 - memos/MemoHook.tsx
@@ -1,17 +1,27 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { MyTitle } from "./MyTitle";
 import { MySubtitle } from "./MySubtitle";
+
+// Outside of the component, we don't need to use useCallback, because it won't be re-rendered
+// const handleCallMyAPI = () => {
+//   console.log("Llamar a mi API");
+// };
 
 export const MemoHook = () => {
   const [myTitle, setMyTitle] = useState("Hola Mundo");
   const [mySubtitle, setMySubytitle] = useState("Hello World");
+
+  // Inside of the component, we need to use useCallback, because if not, it will be re-rendered
+  const handleCallMyAPI = useCallback(() => {
+    console.log("Llamar a mi API", mySubtitle);
+  }, [mySubtitle]);
 
   return (
     <div className="bg-gradient flex flex-col gap-4">
       <h1 className="text-2xl font-thin text-white">MemoApp</h1>
 
       <MyTitle title={myTitle} />
-      <MySubtitle subtitle={mySubtitle} />
+      <MySubtitle subtitle={mySubtitle} callMyAPI={handleCallMyAPI} />
 
       <button
         className="bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer"
--- 04-hooks-app/src/06 - memos/MySubtitle.tsx
+++ 04-hooks-app/src/06 - memos/MySubtitle.tsx
@@ -2,16 +2,20 @@ import { memo } from "react";
 
 interface Props {
   subtitle: string;
+  callMyAPI: () => void;
 }
 
-export const MySubtitle = memo(({ subtitle }: Props) => {
+export const MySubtitle = memo(({ subtitle, callMyAPI }: Props) => {
   console.log("MySubtitle re-rendering");
 
   return (
     <>
       <h6 className="text-2xl font-bold">{subtitle}</h6>
 
-      <button className="bg-indigo-500 text-white px-2 py-2 rounded-md cursor-pointer">
+      <button
+        className="bg-indigo-500 text-white px-2 py-2 rounded-md cursor-pointer"
+        onClick={callMyAPI}
+      >
         Llamar a funcion
       </button>
     </>
