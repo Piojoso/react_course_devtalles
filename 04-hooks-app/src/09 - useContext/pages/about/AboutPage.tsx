e03f9a5484c0e5c222fa854ec2a85d39243d7d06
Piojoso <work.leonel.bongiovanni@outlook.com.ar>
2026-04-20 23:30:37 -0300

    10 - using Link to navigate between pages

--- 04-hooks-app/src/09 - useContext/pages/about/AboutPage.tsx
+++ 04-hooks-app/src/09 - useContext/pages/about/AboutPage.tsx
@@ -1,3 +1,18 @@
+import { Link } from "react-router";
+
 export const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold">About me</h1>
+      <hr />
+      <div className="flex flex-col gap-2 my-10">
+        <Link to="/profile" className="hover:text-blue-500 underline text-2xl">
+          Profile
+        </Link>
+        <Link to="/login" className="hover:text-blue-500 underline text-2xl">
+          Login
+        </Link>
+      </div>
+    </div>
+  );
 };
--- 04-hooks-app/src/09 - useContext/pages/auth/LoginPage.tsx
+++ 04-hooks-app/src/09 - useContext/pages/auth/LoginPage.tsx
@@ -1,3 +1,22 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Link } from "react-router";
+
 export const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold">Login</h1>
+      <hr />
+
+      <form className="flex flex-col gap-2 my-10">
+        <Input type="number" placeholder="user ID" />
+
+        <Button type="submit">Login</Button>
+      </form>
+
+      <Link to="/">
+        <Button variant="ghost">Go back</Button>
+      </Link>
+    </div>
+  );
 };
