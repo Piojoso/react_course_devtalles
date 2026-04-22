fb1703e5b53c225ec33a7648072a02d771b5e577
Piojoso <work.leonel.bongiovanni@outlook.com.ar>
2026-04-21 22:23:32 -0300

    10 - creating user context

--- 04-hooks-app/src/09 - useContext/context/UserContext.tsx
+++ 04-hooks-app/src/09 - useContext/context/UserContext.tsx
@@ -1,11 +1,50 @@
-import { type PropsWithChildren } from "react";
+import { useState, createContext, type PropsWithChildren } from "react";
+import type { User } from "../data/user-mock.data";
 
 // interface Props {
 //   children: React.ReactNode;
 // }
 
-export const UserContextProvider = ({ children }: PropsWithChildren) => {
-  // const [name, setName] = useState("Leonel");
+type AuthStatus = "checking" | "authenticated" | "not-authenticated";
 
-  return <>{children}</>;
+interface UserContextProps {
+  // state
+  authStatus: AuthStatus;
+  user: User | null;
+
+  // methods
+  login: (userId: number) => boolean;
+  logout: () => void;
+}
+
+export const UserContext = createContext<UserContextProps>(
+  {} as UserContextProps,
+);
+
+export const UserContextProvider = ({ children }: PropsWithChildren) => {
+  const [authStatus, setAuthStatus] = useState<AuthStatus>("checking");
+  const [user, setUser] = useState<User | null>(null);
+
+  const handleLogin = (userId: number) => {
+    console.log({ userId });
+
+    return true;
+  };
+
+  const handleLogout = () => {
+    console.log("Logout");
+  };
+
+  return (
+    <UserContext
+      value={{
+        authStatus,
+        user,
+        login: handleLogin,
+        logout: handleLogout,
+      }}
+    >
+      {children}
+    </UserContext>
+  );
 };
