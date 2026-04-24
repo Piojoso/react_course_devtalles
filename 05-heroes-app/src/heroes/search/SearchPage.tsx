72f7de177443d8d9615ca38a9ea7bc4d4e7a6bc2
Piojoso <work.leonel.bongiovanni@outlook.com.ar>
2026-04-23 22:27:15 -0300

    11 - SearchControl component

--- 05-heroes-app/src/heroes/search/SearchPage.tsx
+++ 05-heroes-app/src/heroes/search/SearchPage.tsx
@@ -1,15 +1,21 @@
 import { CustomHeader } from "@/components/custom/CustomHeader";
 import { HeroStats } from "../components/HeroStats";
+import { SearchControls } from "./ui/SearchControls";
 
 export const SearchPage = () => {
   return (
     <>
+      {/* Header */}
       <CustomHeader
         title="Search superheroes"
         subtitle="Search superheroes or villians"
       />
 
+      {/* Stats Dashboard */}
       <HeroStats />
+
+      {/* Controls */}
+      <SearchControls />
     </>
   );
 };
