15e1d507a96a9a575d309cc910acd9debaf17dcc
Piojoso <work.leonel.bongiovanni@outlook.com.ar>
2026-04-28 13:00:59 -0300

    12 - queryKey properly configurated

--- 05-heroes-app/src/heroes/pages/home/HomePage.tsx
+++ 05-heroes-app/src/heroes/pages/home/HomePage.tsx
@@ -23,7 +23,7 @@ export const HomePage = () => {
   }, [activeTab]);
 
   const { data: heroesResponse, isLoading: isHeroesLoading } = useQuery({
-    queryKey: ["heores"],
+    queryKey: ["heores", { page: pageParam, limit: limitParam }],
     queryFn: () => getHeroesByPageAction(+pageParam, +limitParam),
     staleTime: 1000 * 60 * 5,
   });
