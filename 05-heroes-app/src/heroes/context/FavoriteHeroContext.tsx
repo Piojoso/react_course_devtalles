703ba51505e7c703d3fe74dd0be8dcd6cfeaa1f1
Piojoso <work.leonel.bongiovanni@outlook.com.ar>
2026-05-03 22:35:33 -0300

    13 - FavoriteHeroContext

---
+++ 05-heroes-app/src/heroes/context/FavoriteHeroContext.tsx
@@ -0,0 +1,43 @@
+import { createContext, useState, type PropsWithChildren } from "react";
+import type { Hero } from "../interfaces/hero.interface";
+
+interface FavoriteHeroContext {
+  // State
+  favorites: Hero[];
+  favoriteCount: number;
+
+  // Methods
+  toggleFavorite: (hero: Hero) => void;
+  isHeroFavorite: (hero: Hero) => boolean;
+}
+
+export const FavoriteHeroContext = createContext({} as FavoriteHeroContext);
+
+export const FavoriteHeroProvider = ({ children }: PropsWithChildren) => {
+  const [favorites, setFavorites] = useState<Hero[]>([]);
+
+  const toggleFavorite = (hero: Hero) => {
+    const heroExist = favorites.find((h) => h.id === hero.id);
+
+    if (heroExist) {
+      setFavorites(favorites.filter((h) => h.id !== hero.id));
+    } else {
+      setFavorites([...favorites, hero]);
+    }
+  };
+
+  return (
+    <FavoriteHeroContext
+      value={{
+        // state
+        favorites: [],
+        favoriteCount: 0,
+        // methods
+        isHeroFavorite: (hero) => true,
+        toggleFavorite,
+      }}
+    >
+      {children}
+    </FavoriteHeroContext>
+  );
+};
