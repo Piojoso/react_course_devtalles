00a79edafd2fd8af7d4cf624599a74102d991c88
Piojoso <work.leonel.bongiovanni@outlook.com.ar>
2026-05-07 16:03:30 -0300

    13 - useRef and useSearchParam

--- 05-heroes-app/src/heroes/pages/search/ui/SearchControls.tsx
+++ 05-heroes-app/src/heroes/pages/search/ui/SearchControls.tsx
@@ -1,8 +1,23 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Search, Filter, SortAsc, Grid, Plus } from "lucide-react";
+import { useRef } from "react";
+import { useSearchParams } from "react-router";
 
 export const SearchControls = () => {
+  const [searchParam, setSearchParam] = useSearchParams();
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const onKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key === "Enter") {
+      setSearchParam((prev) => {
+        prev.set("name", inputRef.current?.value ?? "");
+
+        return prev;
+      });
+    }
+  };
+
   return (
     <>
       <div className="flex flex-col lg:flex-row gap-4 mb-8">
@@ -10,8 +25,11 @@ export const SearchControls = () => {
         <div className="relative flex-1">
           <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-5 w-5" />
           <Input
+            ref={inputRef}
             placeholder="Search heroes, villains, powers, teams..."
             className="pl-12 h-12 text-lg bg-white"
+            onKeyDown={onKeyDown}
+            defaultValue={searchParam.get("name") ?? ""}
           />
         </div>
 
