a369f72159fab0c5602852297fdb390fab02094a
Piojoso <work.leonel.bongiovanni@outlook.com.ar>
2026-05-15 09:19:07 -0300

    14 - app.router.tsx hero-page and search-page path tested

--- 05-heroes-app/src/router/app.router.test.tsx
+++ 05-heroes-app/src/router/app.router.test.tsx
@@ -1,10 +1,13 @@
 import { describe, expect, test, vi } from "vitest";
 import { appRouter } from "./app.router";
 import { render, screen } from "@testing-library/react";
-import { Outlet, RouterProvider } from "react-router";
+import {
+  createMemoryRouter,
+  Outlet,
+  RouterProvider,
+  useParams,
+} from "react-router";
 
-// vi.mock("@/admin/layouts/AdminLayout");
-// vi.mock("@/admin/pages/AdminPage");
 vi.mock("@/heroes/layouts/HeroesLayout", () => ({
   HeroesLayout: () => (
     <div data-testid="heroes-layout">
@@ -16,8 +19,17 @@ vi.mock("@/heroes/pages/home/HomePage", () => ({
   HomePage: () => <div data-testid="home-page"></div>,
 }));
 vi.mock("@/heroes/pages/hero/HeroPage", () => ({
-  HeroPage: () => <div data-testid="hero-page"></div>,
+  HeroPage: () => {
+    const { idSlug = "" } = useParams();
+
+    return <div data-testid="hero-page">idSlug - {idSlug}</div>;
+  },
 }));
+vi.mock("@/heroes/pages/search/SearchPage", () => ({
+  default: () => <div data-testid="search-page"></div>,
+}));
+// vi.mock("@/admin/layouts/AdminLayout");
+// vi.mock("@/admin/pages/AdminPage");
 
 describe("app.router.tsx", () => {
   test("should be configured as expected", () => {
@@ -27,9 +39,37 @@ describe("app.router.tsx", () => {
     expect(appRouter.routes.at(2)?.path).toBe("*");
   });
 
-  test("should render HomePage at root path", () => {
-    render(<RouterProvider router={appRouter} />);
+  test("should render HomePage at / path", () => {
+    const router = createMemoryRouter(appRouter.routes, {
+      initialEntries: ["/"],
+    });
+
+    render(<RouterProvider router={router} />);
 
     expect(screen.getByTestId("home-page")).toBeDefined();
+  });
+
+  test("should render HeroPage at /heroes/:idSlug path", () => {
+    const idSlug = "clark-kent";
+    const router = createMemoryRouter(appRouter.routes, {
+      initialEntries: [`/heroes/${idSlug}`],
+    });
+
+    render(<RouterProvider router={router} />);
+
+    const heroPageComponent = screen.getByTestId("hero-page");
+    expect(heroPageComponent).toBeDefined();
+    expect(heroPageComponent.innerHTML).toContain(idSlug);
+  });
+
+  test("should render SearchPage at /search path", async () => {
+    const router = createMemoryRouter(appRouter.routes, {
+      initialEntries: [`/search`],
+    });
+    render(<RouterProvider router={router} />);
+
+    expect(await screen.findByTestId("search-page")).toBeDefined();
+    // expect(heroPageComponent).toBeDefined();
+    // expect(heroPageComponent.innerHTML).toContain(idSlug);
   });
 });
