a6a39fd1f0cf5f8d5ac28f102e9a89464ac5646a
Piojoso <work.leonel.bongiovanni@outlook.com.ar>
2026-05-15 14:52:18 -0300

    14 - SearchPage params and heroes render tested

--- 05-heroes-app/src/heroes/pages/search/SearchPage.test.tsx
+++ 05-heroes-app/src/heroes/pages/search/SearchPage.test.tsx
@@ -1,4 +1,4 @@
-import { describe, expect, test, vi } from "vitest";
+import { beforeEach, describe, expect, test, vi } from "vitest";
 import { render, screen, waitFor } from "@testing-library/react";
 
 import { MemoryRouter } from "react-router";
@@ -9,12 +9,23 @@ import { searchHeroesAction } from "@/heroes/actions/search-heroes.action";
 import type { Hero } from "@/heroes/interfaces/hero.interface";
 
 vi.mock("@/heroes/components/HeroGrid", () => ({
-  HeroesGrid: () => <div data-testid="hero-grid"></div>,
+  HeroesGrid: ({ heroes }: { heroes: Hero[] }) => (
+    <div data-testid="heroes-grid">
+      {heroes.map((h) => (
+        <div key={h.id}>{h.alias}</div>
+      ))}
+    </div>
+  ),
 }));
+
+const mockedSearchHeroesActionResponse = [
+  { id: "1", alias: "Superman" },
+  { id: "2", alias: "Batman" },
+] as Hero[];
 
 vi.mock("@/heroes/actions/search-heroes.action");
 const mockedSearchHeroesAction = vi.mocked(searchHeroesAction);
-mockedSearchHeroesAction.mockResolvedValue([{ id: "1" } as Hero]);
+mockedSearchHeroesAction.mockResolvedValue(mockedSearchHeroesActionResponse);
 
 const renderSearchPage = (initialEntries: string = "") => {
   const queryClient = new QueryClient();
@@ -37,6 +48,10 @@ const fullRenderedSearchPage = async (initialEntries: string = "") => {
 };
 
 describe("SearchPage.tsx", () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
   test("should render SearchPage with default values", async () => {
     await fullRenderedSearchPage();
 
@@ -44,5 +59,46 @@ describe("SearchPage.tsx", () => {
       name: "",
       strength: "",
     });
+  });
+
+  test("should call searchHeroesAction with name parameter", () => {
+    const mockedName = "superman";
+    renderSearchPage(`/?name=${mockedName}`);
+
+    expect(mockedSearchHeroesAction).toHaveBeenCalledWith({
+      name: mockedName,
+      strength: "",
+    });
+  });
+
+  test("should call searchHeroesAction with strength parameter", () => {
+    const mockedStrength = "1";
+    renderSearchPage(`/?min-strength=${mockedStrength}`);
+
+    expect(mockedSearchHeroesAction).toHaveBeenCalledWith({
+      name: "",
+      strength: mockedStrength,
+    });
+  });
+
+  test("should call searchHeroesAction with both parameters", () => {
+    const mockedName = "superman";
+    const mockedStrength = "1";
+    renderSearchPage(`/?name=${mockedName}&min-strength=${mockedStrength}`);
+
+    expect(mockedSearchHeroesAction).toHaveBeenCalledWith({
+      name: mockedName,
+      strength: mockedStrength,
+    });
+  });
+
+  test("should render searchHeroesAction response on HeroGrid", async () => {
+    await fullRenderedSearchPage();
+
+    const heroGridElement = screen.getByTestId("heroes-grid");
+
+    expect(heroGridElement.children.length).toBe(
+      mockedSearchHeroesActionResponse.length,
+    );
   });
 });
