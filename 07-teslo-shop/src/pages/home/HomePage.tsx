eab8adc8ec833e8cf2292908ab5d2cb01b9e46e4
Piojoso <work.leonel.bongiovanni@outlook.com.ar>
2026-05-20 23:07:59 -0300

    16 - installed shadcn and tailwind

--- 07-teslo-shop/src/pages/home/HomePage.tsx
+++ 07-teslo-shop/src/pages/home/HomePage.tsx
@@ -1,5 +1,13 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
 
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h1>Hola Mundo</h1>
+      <h1 className="font-montserrat font-thin">Hola Mundo</h1>
+      <h1 className="font-montserrat font-normal">Hola Mundo</h1>
+      <h1 className="font-montserrat font-bold">Hola Mundo</h1>
+      <Button> Hola Mundo </Button>
+    </div>
+  );
 };
