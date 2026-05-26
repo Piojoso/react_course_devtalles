1b24a93ea20c6a205ecc21604cf18e42fed47dfa
Piojoso <work.leonel.bongiovanni@outlook.com.ar>
2026-05-26 00:28:06 -0300

    16 - GenderPage content

--- 07-teslo-shop/src/pages/gender/GenderPage.tsx
+++ 07-teslo-shop/src/pages/gender/GenderPage.tsx
@@ -1,5 +1,32 @@
-import React from "react";
+import { CustomPagination } from "@/components/custom/CustomPagination";
+import { products } from "@/mocks/products.mock";
+import { CustomJumbotron } from "@/shop/components/CustomJumbotron";
+import { ProductsGrid } from "@/shop/components/ProductsGrid";
+import { useParams } from "react-router";
+
+const getGenderLabel = (gender: string | undefined) => {
+  switch (gender) {
+    case "men":
+      return "Hombres";
+    case "women":
+      return "Mujeres";
+    case "kids":
+      return "Niños";
+    default:
+      return "Todos";
+  }
+};
 
 export const GenderPage = () => {
-  return <div>GenderPage</div>;
+  const { gender } = useParams();
+
+  return (
+    <>
+      <CustomJumbotron title={`Productos para ${getGenderLabel(gender)}`} />
+
+      <ProductsGrid products={products} />
+
+      <CustomPagination totalPages={7} />
+    </>
+  );
 };
