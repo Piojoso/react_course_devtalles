520c0e309404f67ca9fdc8a71558a9c9187f61bc
Piojoso <work.leonel.bongiovanni@outlook.com.ar>
2026-06-11 22:36:34 -0300

    16 - AdminSidebar navigations

--- 07-teslo-shop/src/admin/components/AdminSidebar.tsx
+++ 07-teslo-shop/src/admin/components/AdminSidebar.tsx
@@ -3,15 +3,12 @@ import {
   Home,
   Users,
   BarChart3,
-  Settings,
-  FileText,
   ShoppingCart,
-  Bell,
-  HelpCircle,
   ChevronLeft,
   ChevronRight,
 } from "lucide-react";
 import { CustomLogo } from "@/components/custom/CustomLogo";
+import { Link, useLocation } from "react-router";
 
 interface SidebarProps {
   isCollapsed: boolean;
@@ -19,16 +16,18 @@ interface SidebarProps {
 }
 
 const AdminSidebar: React.FC<SidebarProps> = ({ isCollapsed, onToggle }) => {
+  const { pathname } = useLocation();
+
   const menuItems = [
-    { icon: Home, label: "Dashboard", active: true },
-    { icon: BarChart3, label: "Analytics" },
-    { icon: Users, label: "Users" },
-    { icon: ShoppingCart, label: "Orders" },
-    { icon: FileText, label: "Reports" },
-    { icon: Bell, label: "Notifications" },
-    { icon: Settings, label: "Settings" },
-    { icon: HelpCircle, label: "Help" },
+    { icon: Home, label: "Dashboard", to: "/admin" },
+    { icon: BarChart3, label: "Productos", to: "/admin/products" },
+    { icon: Users, label: "Usuarios" },
+    { icon: ShoppingCart, label: "Ordenes" },
   ];
+
+  const isRouteActive = (to = "") => {
+    return to === pathname;
+  };
 
   return (
     <div
@@ -54,10 +53,10 @@ const AdminSidebar: React.FC<SidebarProps> = ({ isCollapsed, onToggle }) => {
             const Icon = item.icon;
             return (
               <li key={index}>
-                <a
-                  href="#"
+                <Link
+                  to={item.to || "/admin"}
                   className={`flex items-center space-x-3 px-3 py-2 rounded-lg transition-all duration-200 group ${
-                    item.active
+                    isRouteActive(item.to)
                       ? "bg-blue-50 text-blue-600 border-r-2 border-blue-600"
                       : "text-gray-600 hover:bg-gray-50 hover:text-gray-900"
                   }`}
@@ -66,7 +65,7 @@ const AdminSidebar: React.FC<SidebarProps> = ({ isCollapsed, onToggle }) => {
                   {!isCollapsed && (
                     <span className="font-medium">{item.label}</span>
                   )}
-                </a>
+                </Link>
               </li>
             );
           })}
